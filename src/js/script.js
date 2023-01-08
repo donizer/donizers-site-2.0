425a6c93e66f9cdd8986eb1e1b8d95b55ee57daa
donizer <bogdan.ua45@gmail.com>
2023-01-08 20:33:03 +0200

    #

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 function burgerButton(e) {
-	let nav = document.getElementById("nav");
+	let nav = document.getElementById('nav');
 	console.log(e.classList.length);
 	nav.classList.toggle('active');
 	e.classList.toggle('active');
-
 }
