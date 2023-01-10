dcb1b303fa70ad1625fe9958889a954f6e0aa358
donizer <bogdan.ua45@gmail.com>
2023-01-10 18:02:17 +0200

    complete mobile layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,23 @@
+let nav = document.getElementById('nav');
+let mBack = document.getElementById('mBack');
+let burgerBtn = document.getElementById('burgerBtn');
+let main = document.getElementsByTagName("main")[0];
+
 function burgerButton() {
-	let nav = document.getElementById('nav');
-	let mBack = document.getElementById('mBack');
-	let burgerBtn = document.getElementById('burgerBtn');
+
 	nav.classList.toggle('active');
 	mBack.classList.toggle('active');
 	burgerBtn.classList.toggle('active');
 }
+
+document.addEventListener('swiped-left', function(e) {
+	main.classList.remove('active');
+	console.log(123)
+	
+});
+
+document.addEventListener('swiped-right', function(e) {
+	main.classList.add('active');
+	console.log(123)
+	
+});
