1d94f863b9fdcfe7a9a62917ee6b8c474e46071d
donizer <bogdan.ua45@gmail.com>
2023-01-29 16:16:17 +0200

    #

---
+++ polygon/random_waifu/waifu.js
@@ -0,0 +1,16 @@
+link = 'https://api.waifu.pics/sfw/neko';
+id = document.getElementById('img')
+
+async function getJSON(url) {
+	const response = await fetch(url);
+	return response.json();
+}
+
+
+async function findWaifu(e) {
+	await getJSON(link).then(response => {
+		e.src = response['url']
+	})
+}
+
+findWaifu(id);
